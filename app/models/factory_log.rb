f8fa3ac6a5bf5d7b59f976bbb77f05e6b023f6a7
yoshitake.nakaji <kokodoko966@gmail.com>
2015-05-27 17:51:40 +0900

    fix hound

--- app/models/factory_log.rb
+++ app/models/factory_log.rb
@@ -8,18 +8,21 @@ class FactoryLog < ActiveRecord::Base
   def self.time_ranking
     ranking = []
     FactoryLog.all.each do |log|
-      if index = ranking.index{|cd| cd[:name] == (log.name + log.traits)}
+      time = log.time
+      name = log.name
+      traits = log.traits
+      if index = ranking.index{ |cd| cd[:name] == (name + traits) }
         hash_data = ranking[index]
-        hash_data[:total_time] += log[:time]
+        hash_data[:total_time] += time
         hash_data[:count] += 1
         hash_data[:average_time] = hash_data[:total_time] / hash_data[:count]
       else
-       ranking.push( {
-                      name: (log.name + log.traits),
-                      total_time:log.time,
+       ranking.push(
+                      name: (name + traits),
+                      total_time: time,
                       count: 1,
-                      average_time: log.time
-                      } )
+                      average_time: time
+                   )
      end
     end
     ranking.sort_by!{|l| l[:total_time]}.reverse
@@ -28,13 +31,13 @@ class FactoryLog < ActiveRecord::Base
   # Return specified factory's average exectuaion time.
   def self.average_time(name_and_traits)
     logs = FactoryLog.log_set_with_trait(name_and_traits)
-    logs.map{ |log| log.time }.inject{ |sum, n| sum + n } / logs.size
+    logs.map(&:time).inject{ |sum, n| sum + n } / logs.size
   end
 
   # Return specified factory's total exectuaion time.
   def self.sum_time(name_and_traits)
     log_set_with_trait(name_and_traits).map(&:time).inject do |log1, log2|
-       log1 + log2
+      log1 + log2
     end
   end
 
@@ -44,12 +47,10 @@ class FactoryLog < ActiveRecord::Base
     FactoryLog.all.each do |log|
       factory_name = log.name
       factory_traits = log.traits
-      unless naming_candidates.any? do |cd|
-         cd == (factory_name + factory_traits)
-       end
-       naming_candidates.push(factory_name + factory_traits)
-     end
-     end
+      unless naming_candidates.include?(factory_name + factory_traits)
+        naming_candidates.push(factory_name + factory_traits)
+      end
+    end
     naming_candidates
   end
 
