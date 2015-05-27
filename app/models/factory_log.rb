3c511d92eac529fcbe4cadf6cfa3b6d7ec8f72e5
yoshitake.nakaji <kokodoko966@gmail.com>
2015-05-27 17:05:08 +0900

    write tests and documents

--- app/models/factory_log.rb
+++ app/models/factory_log.rb
@@ -2,6 +2,8 @@ class FactoryLog < ActiveRecord::Base
   #TODO: fix algorithm
   #TODO: fix name devide to name and traits
   #TODO: fix visualize association relation and selectable counting or not.
+
+  #Return Factory's log ranking about total time it took. They are identified factory_name + trait_name.
   def self.time_ranking
     ranking = []
     FactoryLog.all.each do |log|
@@ -16,29 +18,31 @@ class FactoryLog < ActiveRecord::Base
     ranking.sort_by!{|l| l[:total_time]}.reverse
   end
 
-  def self.average_time(factory)
-    sum_time(factory) / log_set_with_trait(factory).size
+  #Return specified factory's average exectuaion time.
+  def self.average_time(factory_name_and_traits)
+    logs = FactoryLog.log_set_with_trait(factory_name_and_traits)
+    logs.map{ |log| log.time }.inject{ |sum, n| sum + n } / logs.size
   end
 
-  def self.sum_time(factory)
-    log_set_with_trait(factory).map{|f| f.time}.inject{|fl1, fl2| fl1 + fl2 }
+  #Return specified factory's total exectuaion time.
+  def self.sum_time(factory_name_and_traits)
+    log_set_with_trait(factory_name_and_traits).map{|f| f.time}.inject{|fl1, fl2| fl1 + fl2 }
   end
 
+  #Return all fatcory_name + trait_name convinaitons in your factoy_logs table.
   def self.candidates
     naming_candidates = []
-    FactoryLog.all.each{|log| naming_candidates.push(log.name + log.traits) unless naming_candidates.any?{|cd| cd ==(log.name + log.traits)}}
+    FactoryLog.all.each{|log| naming_candidates.push(log.name + log.traits) unless naming_candidates.any?{ |cd| cd ==(log.name + log.traits) } }
     naming_candidates
   end
 
-  def self.log_set_with_trait(factory_with_trait)
-    factory_set = []
-    FactoryLog.all.each{|log| factory_set.push(log) if log.name + log.traits == factory_with_trait}
-    factory_set
+  #Return all specified factory object(identified String :"factory_name + trait_name")
+  def self.log_set_with_trait(factory_name_and_trait)
+    FactoryLog.all.select{ |log| log.name + log.traits == factory_name_and_trait }
   end
 
+  #Return all specified factory object(identified factory_name only)
   def self.log_set(factory)
-    factory_set = []
-    FactoryLog.all.each{|log| factory_set.push(log) if log.name == factory}
-    factory_set
+    FactoryLog.all.select{|log| log.name == factory }
   end
 end
