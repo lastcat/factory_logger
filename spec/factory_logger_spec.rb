b39f48d789744bc5d71787b7120f71b1d063a1e1
yoshitake.nakaji <kokodoko966@gmail.com>
2015-05-28 23:35:39 +0900

    fix hound [ci skip]

--- spec/factory_logger_spec.rb
+++ spec/factory_logger_spec.rb
@@ -1,10 +1,10 @@
-require 'spec_helper'
+require "spec_helper"
 describe FactoryLogger do
   describe "is configurable" do
     FactoryLogger.configure do |config|
       config.something = "something config"
     end
-    it 'configured' do
+    it "configured" do
       expect(FactoryLogger.config.something).to eq ("something config")
     end
   end
