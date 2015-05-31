047c026a60c49a48a5842f4d11d55c861c10d310
yoshitake.nakaji <kokodoko966@gmail.com>
2015-05-31 12:49:51 +0900

    add asso model and spec

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529054255) do
+ActiveRecord::Schema.define(version: 20150530075825) do
 
   create_table "asso_relations", force: :cascade do |t|
     t.integer  "factory_id"
